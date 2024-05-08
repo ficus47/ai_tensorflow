272524ae420076aa7a5c21f4beebf557ae558c38
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-05-08 23:16:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import train
 
 st.title("AI on Replit")
 
-file1 = "maman\ngentil\nimbecile\ndebile"
-file2 = "neutre\nneutre\nvulgaire\nvulgaire"
+file1 = uploaded_file = st.file_uploader("Charger un fichier .txt", type=["txt"])
+ #"maman\ngentil\nimbecile\ndebile"
+file2 = uploaded_file = st.file_uploader("Charger un fichier .txt", type=["txt"])
+#"neutre\nneutre\nvulgaire\nvulgaire"
 
 epochs = st.slider(label="Epochs", min_value=1, max_value=100, value=10)
 lr = st.number_input(label="Learning rate va etre divisé par 10", min_value=0.0001, max_value=1.0, value=0.0001)
@@ -14,4 +16,4 @@ lr = st.number_input(label="Learning rate va etre divisé par 10", min_value=0.0
 st.write(f"learning rate = {lr}")
 
 if file1 is not None and file2 is not None and st.button("train"):
-  train.train(file1, file2, epochs, lr)
+  train.train(file1.read().decode(´utf-8'), file2.read().decode(´utf-8'), epochs, lr)
